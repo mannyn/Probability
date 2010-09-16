ab4885fad39eea391c8959c7216c26068b52ace9
Jason Brownlee <jason.brownlee05@gmail.com>
2010-09-16 17:05:44 +1000

    updated comments

--- src/prob/Bayes.java
+++ src/prob/Bayes.java
@@ -2,23 +2,26 @@ package prob;
 
 /**
  * Implementation of Bayes 
- *
+ * Some lame attempts at automating some of the computation involved
+ * 
+ * p(A | B) = p(B | A) p(A) / p(B)
+ * p(Hypothesis | Data) = p(Data | Hypothesis) p(Hypothesis) / p(Data)
+ * 
+ * p(B | A) = conditional probability of A, given B
+ * p(A) = prior probability or marginal probability of A
+ * p(B) = prior or marginal probability of B
+ * 
+ * 
  * http://jsteinhardt.wordpress.com/2010/09/13/nobody-understands-probability/
  * http://en.wikipedia.org/wiki/Bayes'_theorem
  * 
  */
 public class Bayes
 {
-	/**
-	 * p(A | B) = p(B | A) p(A) / p(B)
-	 * p(Hypothesis | Data) = p(Data | Hypothesis) p(Hypothesis) / p(Data)
-	 * 
-	 * @param a
-	 * @param b
-	 * @return
-	 */
+	
 	public final static double pAGivenB(double a, double b)
 	{
+		// can this be normalized? 
 		return 0;
 	}
 	
